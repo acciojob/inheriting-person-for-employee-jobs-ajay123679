6484c2d76cc1b5c7ced76d9a76a3d2bdf9f92cfa
ajay123679 <65329197+ajay123679@users.noreply.github.com>
2025-05-12 10:25:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 // complete this js code
-function Person(name, age) {}
-
-function Employee(name, age, jobTitle) {}
-
+class Person{
+	constructor(name,age)
+	{
+		this.name=name;
+		this age=age;
+	}
+	greet(){
+		console.log("Hello, my name is "+this.name+", I am "+this.age+" years old.")
+	}
+}
+class Employee{
+	super(name,age);
+	this.jobTitle=jobTitle;
+	jobGreet(){
+		console.log("Hello, my name is "+this.name+", I am "+this.age+" years old, and my job title is "+this.jobTitle);
+    }
+}
+const person=new Person("ALice",25);
+person.greet();
+const employee=new Employee("Bob",30,"Manager");
+employee.jobGreet();
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
